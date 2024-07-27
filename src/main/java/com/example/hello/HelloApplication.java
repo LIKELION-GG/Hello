4a7e858a3c110917ae2bef6e5cb3530e750432e3
leeesoraaa <lsrlsh9981@gmail.com>
2024-07-28 05:41:05 +0900

    test4 commit

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Test");
+		System.out.println("Hello World Test2");
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
