e284365cb05c330c9f675240e5a641f0bd3d1971
leeesoraaa <lsrlsh9981@gmail.com>
2024-07-28 05:52:12 +0900

    HelloWorld

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -2,13 +2,25 @@ package com.example.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Test2");
 		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@GetMapping(value = "/")
+	public String doGetHelloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping(value = "/korea")
+	public String doGetHelloKorea() {
+		return "Hello Korea";
 	}
 
 }
